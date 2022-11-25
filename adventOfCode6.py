5b655ac91952ce4bdd6e6e4ae71e9adb9f57f355
Kacper-W-Kozdon <k.kozdon@gmail.com>
2022-11-25 22:32:04 +0100

    day 6 part 2

--- adventOfCode6.py
+++ adventOfCode6.py
@@ -7,6 +7,7 @@ Created on Fri Nov 25 16:30:15 2022
 
 import numpy as np
 import re
+import time
 
 def load_files():
     fContent = []
@@ -18,22 +19,23 @@ def load_files():
     return(fContent, fContent.shape)
 
 def n_new_fish(tab):
-    num_new_fish = sum((tab == 0).astype(int))
+    num_new_fish = sum((tab == 0).astype(np.ushort))
     # print("No. of new fish2: ", num_new_fish)
     return num_new_fish
 
 def add_new_fish(tab):
-    new_fish_cyc = 8
+    new_fish_cyc = np.ushort(8)
     num_new_fish = n_new_fish(tab)
     # print(len(tab))
-    tab = np.append(tab, [new_fish_cyc for index in range(num_new_fish)])
+    tab = np.append(tab, [new_fish_cyc for index in range(num_new_fish)]).astype(np.ushort)
     # print(len(tab))
-    # print("No. of new fish3: ", len([new_fish_cyc for index in range(num_new_fish)]))
+    # print("No. of new fish3: ", .cclen([new_fish_cyc for index in range(num_new_fish)]))
     return tab
 
 def age_fish(tab, maxage = 81):
     newage = maxage - 1
     notParents = np.where(tab != 0)
+    # print(notParents) if newage == 80 else 0
     if newage == 0:
         # print(newage)
         return tab
@@ -53,64 +55,68 @@ def flatten(l):
 
 def age_fish2(tabs, maxage = 81):
     for age in range(maxage):
+        t1 = time.time()
         print("age: ", age)
         age = maxage - 1
-        maxsize = 512
+
         # print(tabs)
-        tabsClone = np.copy(tabs)
-        
-        for (index, tab) in enumerate(tabsClone):
-            tab = np.array(tab)
+        # tabsSize = 0
+        # for elem in tabs:
+            # tabsSize = tabsSize + 1
+        # for index in range(tabsSize):
             # print(np.array(tab) != 0)
-            notParents = np.where(tab != 0)
+        notParents = np.where(np.array(tabs, dtype = int) != 0)
             # print(notParents)
-            if 0 in tab:
+            # print(notParents)
+        if 0 in tabs:
                 # print("0 in tab")
                 # print("No. of new fish1: ", sum((tab == 0).astype(int)))
-                tab = add_new_fish(tab)
+            new_fishes = tabs.count(0)
+            # print(new_fishes)
+                # print(tabs)
+            for i in range(new_fishes):
+                tabs.append(np.short(8))
                 # print(tab)
                 # print(len(tab[np.where(tab == 0)]))
-                tab[np.where(tab == 0)] = 6
+                # print(np.where(tabs == 0))
+                # print(tabs)
+                # print(np.where(np.array(tabs) == 0))
+            for elem in np.where(tabs == 0)[0]:
+                tabs[elem] = np.ushort(6)
                 # print(len(tab[np.where(tab == 6)]))
             # print("shape: ", tab.shape)
-            tab[notParents] = tab[notParents] - 1
+        for elem in notParents[0]:
+            tabs[elem] = tabs[elem] - np.ushort(1)
             # tabs = np.array(tabs)
             # print(tabs.shape)
             # print(tabs[0])
             # tabs = np.delete(tabs, [index], 0)
-            tabs.pop(index)
             # print("Shape: ", tabs.shape)
             # tabs = np.append(tabs, np.array([tab]), 0)
             # tabs = tabs.append(tab)
-            tabs.insert(index, tab)
             # print()
             # print(len(tab))
-            if len(tab) > maxsize:
-                # print("New batch")
-                # tabs = np.append(tabs, [tab[maxsize:]])
-                tabs.append(tab[maxsize:])
-                tab = tab[:maxsize]
-                # tabs = np.delete(tabs, index)
-                tabs.pop(index)
-                # tabs = np.append(tabs, np.array([tab]))
-                tabs.insert(index, tab)
-            # tab = age_fish(tab, newage)
-            # print(newage)
+            # size = 0
+            # for idx in tabs[tabsSize:]:
+            #     size = size + 1
+            # tabsSize = tabsSize + size
+        t2 = time.time()
+        print("runtime: ", (t2 - t1))
             
     return tabs
 
 def run1(age):
-    fContent = np.array([load_files()[0]]).tolist()
+    fContent = np.array(load_files()[0]).astype(np.ushort())
     # print(np.where(fContent != 0))
     # print(fContent, load_files()[1])
-    fin_popul = age_fish(tabs = fContent, maxage = age)
+    fin_popul = age_fish(tab = fContent, maxage = age)
     # print(fin_popul)
     fin_pop_size = len(fin_popul)
     print("Population size after " + str(age - 1) + " days: ")
     return fin_pop_size
 
 def run2(age):
-    fContent = np.array([load_files()[0]]).tolist()
+    fContent = np.array(load_files()[0]).astype(np.ushort).tolist()
     # print(np.where(fContent != 0))
     # print(fContent, load_files()[1])
     fin_popul = age_fish2(tabs = fContent, maxage = age)
@@ -118,6 +124,7 @@ def run2(age):
     fin_pop_size = len(fin_popul)
     print("Population size after " + str(age - 1) + " days: ")
     return fin_pop_size
+
 
 print(run1(81))
 print(run2(257))
