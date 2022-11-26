a7e453e5a261a8e94cad8f7ddb56fcadf6d1135f
Kacper-W-Kozdon <k.kozdon@gmail.com>
2022-11-26 22:39:32 +0100

    day 8 part 1

--- adventOfCode8.py
+++ adventOfCode8.py
@@ -54,6 +54,37 @@ def data_processing2(data):
     fContent = np.array(fContent, dtype = object)
     return fContent
 
+def decoding(ins, outs):
+    # print(sorted(ins[0][1]))
+    
+    for (index, line) in enumerate(ins):
+        for (idx, elem) in enumerate(line):
+            tempVal = "".join(sorted(elem))
+            # print(tempVal)
+            line[idx] = tempVal
+            # print(line[idx])
+    # print(ins)
+            
+    for (index, line) in enumerate(outs):
+        for (idx, elem) in enumerate(line):
+            tempVal = "".join(sorted(elem))
+            line[idx] = tempVal
+    
+    listOfNum = []
+    
+    for (index, line) in enumerate(ins):
+        decodedNumber = np.zeros(4)
+        for (idx, elem) in enumerate(line):
+            # print(np.array(outs[index]) == elem)
+            decodedNumber = decodedNumber + np.array(elem == np.array(outs[index])).astype(int) * (9 - idx)
+            # print(decodedNumber)
+            if index == 0:
+                print((9 - idx))
+                print(decodedNumber)
+        listOfNum.append(decodedNumber)
+            
+    return listOfNum
+
 def run():
     fContent = load_files()
     processed_data = data_processing2(data = fContent)
@@ -61,7 +92,33 @@ def run():
     outputs = processed_data[:, 1]
     total_uniques = count_uniques(data = outputs)
     # print(len(outputs[0][1]))
-    return total_uniques
+    decoded = np.array(decoding(ins = inputs, outs = outputs))
+    decoded2 = []
+    for index, number in enumerate(decoded):
+        tempVal = ""
+        for digit in number:
+            tempVal = tempVal + str(int(digit))
+        decoded2.append(tempVal)
+    # print(int("0226"))       
+    # print(decoded2)
+    # print(len(decoded2))
+    # print(len(outputs))
+    # sumN = 0
+    # for el in decoded2:
+        # sumN = sumN + float(el)
+    # print(sumN)
+    result = sum(np.array(decoded2).astype(float))
+    return total_uniques, result
 # print(load_files()[0])
 # print(data_preprocessing(load_files())[1])
 print(run())
+
+
+'''
+Znaleźć cyfry o określonej liczbie kresek i wyznaczyć, która to która 
+w oparciu o przekrycie z unikatowymi (1, 4, 7, 8). Przykładowo:
+2, 5, 3 mają tyle samo kresek
+3 z 8 ma 6, z 7 ma 3, z 4 ma 3, z 1 ma 2
+5 z 8 ma 6, z 7 ma 2, z 4 ma 3, z 1 ma 1
+2 z 8 ma , z 7 ma 2, z 4 ma 2, z 1 ma 1
+'''
